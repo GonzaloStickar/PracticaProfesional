aa1b05ea05b15076526e6863fada0bb6e326d222
Gonzalo <gonzaloostickarr@gmail.com>
2024-07-05 21:08:08 -0300

    estilos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require("express")
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
