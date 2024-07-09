560c28506823e6d8e28395135cc85e5b99cdd3fd
Gonzalo <113151385+GonzaloStickar@users.noreply.github.com>
2024-07-09 15:42:38 -0300

    test deploy

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const { sessionSecret } = require('../controllers/config.js');
 
-const isAuthV2 = (req, res) => {
-	const { cookies } = req;
+const main = {
+    getInicio: (req, res) => {
+        try {
+            const { cookies } = req;
 	if (cookies.session_id) {
 		if (cookies.session_id===sessionSecret) {
             return res.sendFile(path.join(__dirname, '..', 'components', 'indexLogout.htm'));
@@ -12,12 +14,6 @@ const isAuthV2 = (req, res) => {
 	} else {
         return res.sendFile(path.join(__dirname, '..', 'components', 'index.htm'));
     }
-};
-
-const main = {
-    getInicio: (req, res) => {
-        try {
-            return isAuthV2(req, res);
         }
         catch (e) {
             res.send({msg:e});
