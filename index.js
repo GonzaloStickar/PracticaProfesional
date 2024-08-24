fa3f98d090244db4f10b98a85cd612a3c9cbc1ec
Gonzalo <113151385+GonzaloStickar@users.noreply.github.com>
2024-08-24 13:03:43 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require("express")
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -9,6 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
